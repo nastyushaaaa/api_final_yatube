3628bba4557aecc080d27b84cf62fa54728868e3
Nastya Platonova <nastya27platonova@gmail.com>
2025-03-14 21:41:58 +0300

    проект после запуска перед проверкой

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'api',
     'posts',
 ]
